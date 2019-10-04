8a8e6e3663e121049a01870200bbaa32915bff6e
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-04 13:23:41 +0530

    Add home screen

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from './../../components/Aux';
+
+class Home extends React.Component {
+  render() {
+    return(<Aux>
+      <section>
+        <h1>Chess</h1>
+      </section>
+    </Aux>)
+  }
+}
+
+export default Home;
